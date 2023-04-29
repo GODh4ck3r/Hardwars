fdabafbb3f618ca3f9ee430c0ffa36c01bef676c
shivamsuyal <shivamsuyal25@gmail.com>
2023-04-29 20:26:40 +0530

    x

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ app.set('view engine', 'ejs');
 app.use(express.static("assets"))
 
 app.use(express.urlencoded({extended:true}))
-app.use('/',mainRoute)
+app.all('/',(req,res)=>{
+    res.send('ok')
+})
+// app.use('/',mainRoute)
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}/`)
