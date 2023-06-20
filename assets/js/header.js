afafbf186d0a371d83ef197377879fb092fa9c1e
shivamsuyal <shivamsuyal25@gmail.com>
2023-06-20 13:57:30 +0530

    commit

---
+++ assets/js/header.js
@@ -0,0 +1,6 @@
+var navBar = document.querySelector("header");
+var navBarBackLight = document.getElementById("navBarBackLight");
+function menuBarFunc() {
+    navBar.classList.toggle("navResponsive")
+    navBarBackLight.classList.toggle("allHidden")
+}
--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use('/',mainRoute)
 app.use('/api',api)
 
-app.listen(PORT,()=>{
+app.listen(PORT,'0.0.0.0',()=>{
     console.log(`Server is running on http://localhost:${PORT}/`)
 })
 
